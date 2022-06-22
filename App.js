f176e585d1a4522edc49a6aa81c80d9858ec1303
Thomas Deconinck <thomas.deconinck@colisweb.com>
2022-06-22 18:24:29 +0200

    up

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { MMKV, useMMKV, useMMKVString } from "react-native-mmkv";
 
 export default function App() {
+  ``;
+  const defaultStorage = useMMKV();
+  const otherStorage = useMMKV({ id: "test" });
+  const [isOtherStorage, setOtherStorage] = useState(false);
+  const [data, setData] = useMMKVString(
+    "data",
+    isOtherStorage ? otherStorage : defaultStorage
+  );
+  const [dataKey, setDataKey] = useMMKVString(
+    isOtherStorage ? "dataWithKey" : "dataWithoutKey"
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Clear"
+        color={"red"}
+        onPress={(_) => {
+          otherStorage.clearAll();
+          defaultStorage.clearAll();
+        }}
+      />
+      <Button
+        title="Toggle storage"
+        onPress={(_) => {
+          setOtherStorage((v) => !v);
+        }}
+      />
+      <Button
+        onPress={(_) => {
+          setData((v) => Date.now().toString());
+          setDataKey((v) => Date.now().toString());
+        }}
+        title="Set data"
+      />
+
+      <Text>{`Storage : ${isOtherStorage ? "test" : "default"}`}</Text>
+      <Text>{`data with storage : ${data}`}</Text>
+      <Text>{`data with key : ${dataKey}`}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +51,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
